7f5a88c3f412251f5db43e2a632a45a8aa40501a
juan123sebastian <122684235+juan123sebastian@users.noreply.github.com>
2023-03-18 18:13:32 +0000

    Subimos mejor ui y solucion a errores

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const clave = '387c9286c5d84467da3aa810e84db8ec';
 
 const inputEl = document.getElementById('input');
-const buttonEl = document.getElementById('buscar');
+const formularioEl = document.getElementById('buscarContenedor');
 
 const datosEl = document.querySelector('.datos')
 
@@ -12,26 +12,33 @@ const descripcionEl = document.getElementById('descripcion')
 const sensacionTermicaEl = document.getElementById('senter')
 const presionEl = document.getElementById('presion') 
 const errorEl = document.querySelector('.error')
-buttonEl.addEventListener('click', () =>{
+const countryEl = document.getElementById('country')
+formularioEl.addEventListener('submit', e =>{
+    e.preventDefault()
     const ciudad = inputEl.value;
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&q=${ciudad}&lang=es&units=metric&appid=${clave}`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?id=524901&q=${ciudad}&lang=es&units=metric&appid=${clave}`)
         .then(Response => Response.json())
         .then(Response =>{
             if(Response.cod <= 300){
-                const {city: {name}} = Response;
+                const {city: {name,country}} = Response;
                 const {main:{feels_like, pressure, temp, humidity}} = Response.list[0];
                 const {description} = Response.list[0].weather[0];
 
                 ciudadEl.innerText = name;
                 temperaturaEl.innerText = Math.floor(temp);
-                descripcionEl.innerHTML = description;
-                sensacionTermicaEl.innerHTML = Math.floor(feels_like);
-                humedadEl.innerHTML = Math.floor(humidity);
-                presionEl.innerHTML = pressure;
+                descripcionEl.innerText = description;
+                sensacionTermicaEl.innerText = Math.floor(feels_like);
+                humedadEl.innerText = Math.floor(humidity);
+                presionEl.innerText = pressure;
+                countryEl.innerText = country 
                 datosEl.style.display="flex"
+                errorEl.style.display="none"
             }else{
-                errorEl.style.display="flex"
+                
             }
         })
-        .catch(err =>console.error(err))
+        .catch(() =>{
+            errorEl.style.display="flex"
+                datosEl.style.display="none"
+        })
 })
